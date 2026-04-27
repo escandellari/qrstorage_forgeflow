e08fb4f4a4832f6d4d3503145246d1ec3fa7488d
Elisa Scandellari <elisa.techmail@gmail.com>
2026-04-27 20:58:11 +0100

    feat: add box QR scan flow

---
+++ app/scan/page.tsx
@@ -0,0 +1,5 @@
+import { BoxScanPage } from '@/src/features/box-scan';
+
+export default function BoxScanRoute() {
+  return <BoxScanPage />;
+}
---
+++ src/features/box-scan/index.ts
@@ -0,0 +1,1 @@
+export { BoxScanPage } from './BoxScanPage';
